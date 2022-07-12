655302869f1b68ef18f2773049959f71847cc568
Elizabeth Adhiambo <adhiamboliz3@gmail.com>
2022-07-12 19:48:09 +0300

    Heroku deployment

--- core/views.py
+++ core/views.py
@@ -1,8 +1,7 @@
-from multiprocessing import context
+from dj_rest_auth.registration.views import RegisterView
 from django.shortcuts import render,redirect
 from django.urls import reverse
 from rest_framework import serializers, generics,viewsets
-from rest_auth.registration.views import RegisterView
 from core.forms import BookAppointmentForm, DoctorProfileUpdateForm, DoctorRegisterForm, LabTestForm, LoginForm, PatientRegisterForm, ProfileUpdateForm, UserUpdateForm
 from django.contrib.auth import login, logout, authenticate
 from core.models import Doctor, Doctorblog, LabTest, Patient, Patientappointment,User
